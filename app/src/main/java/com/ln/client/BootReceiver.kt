f23b3a8bcd536edade81c0d6a5c8f9383f2c26de
luanzzkribeirozzk-design <luanzzkribeirozzk@gmail.com>
2026-03-22 01:41:10 -0300

    feat: upload inicial app-client

---
+++ app/src/main/java/com/ln/client/BootReceiver.kt
@@ -0,0 +1,19 @@
+package com.ln.client
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val service = Intent(context, MonitorService::class.java)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(service)
+            } else {
+                context.startService(service)
+            }
+        }
+    }
+}
